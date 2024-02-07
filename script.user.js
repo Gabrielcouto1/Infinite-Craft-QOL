eabfa0393b152e3c02f2eafe393f2cd1aea14585
Gabriel Couto <gabrielcouto3.14@gmail.com>
2024-02-07 20:43:38 -0300

    test

--- script.user.js
+++ script.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         Infinite-craft quality of life.
+// @name         Infinite craft QOL test
 // @namespace    http://tampermonkey.net/
 // @version      2024-02-07
 // @description  try to take over the world!
 // @author       You
-// @match        https://neal.fun/infinite-craft/
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @match        http://neal.fun/infinite-craft
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
