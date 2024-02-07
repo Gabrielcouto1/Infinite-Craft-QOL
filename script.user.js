bf4bf9eb3c57bd0cba3c4b64bd525705caa416dd
Gabriel Couto <gabrielcouto3.14@gmail.com>
2024-02-07 20:36:37 -0300

    fix: added metadata to user script

--- script.user.js
+++ script.user.js
@@ -1,3 +1,14 @@
+// ==UserScript==
+// @name         Infinite-craft quality of life
+// @namespace    http://tampermonkey.net/
+// @version      2024-02-07
+// @description  try to take over the world!
+// @author       You
+// @match        https://neal.fun/infinite-craft/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
 import {createSortButton} from "./scripts/sort"
 
 createSortButton()
